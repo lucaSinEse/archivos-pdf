fb7ebd5f662580b51738030626b10d361034fcff
leonelminetti <85181312+leonelminetti@users.noreply.github.com>
2024-11-27 12:46:08 -0300

    Completo formulario tipo tramite
    Se agrego el modificar y las validaciones (Testear todo)

--- js/tipoTramites.js
+++ js/tipoTramites.js
@@ -16,6 +16,20 @@ const obtenerTipoTramites = document.getElementById(
 );
 
 
+// elementos formulario editar
+const lblIdTipotramite = document.getElementById(
+  "lblIdTipotramite"
+);
+const descripcionTramiteEditado = document.getElementById(
+  "descripcionTramiteEditado"
+);
+const estadoTramiteEditado = document.getElementById(
+  "estadoTramiteEditado"
+);
+const lblnotificacioneditado = document.getElementById(
+  "lblnotificacioneditado"
+);
+
 
 $(document).ready(function() {
   //trae todos los elemnetos para l
@@ -33,10 +47,17 @@ function cerrarDialog(id) {
   document.getElementById(id).close();
 }
 
-function abrirDialogEditar(idDialog, id_TipoTramite) {
+function abrirDialogEditar(idDialog, id_TipoTramite,estado, descripcion) {
   const dialog = document.getElementById(idDialog);
   dialog.showModal();
-  console.log(id_TipoTramite);
+  /* console.log(id_TipoTramite); */
+  lblIdTipotramite.innerHTML = id_TipoTramite;
+  descripcionTramiteEditado.value = descripcion;
+  if(estado =='1'){
+    estadoTramiteEditado.checked = true;
+  }else{
+    estadoTramiteEditado.checked = false;
+  }
 }
 
 function abrirNav() {
@@ -45,26 +66,31 @@ function abrirNav() {
 }
 
 function AgregarTipoTramite() {
-  $.ajax({
-    type: "POST",
-    url: "./php/phpinserts.php",
-    data: {
-      action: "GuardarTipoTramite",
-      Descripcion: txtdescripcionTramiteCreado.value,
-      estado: cbxestadoTramiteCreado.checked,
-    },
-    success: function (data) {
-      if (data == "esta") {
-        lblnotificacionagregado.innerText = "esta";
-      } else {
-        if (data == "guardado") {
-          lblnotificacionagregado.innerText = "Se guardo correctamente";
+  if(txtdescripcionTramiteCreado.value ==""){
+    lblnotificacionagregado.innerText = "complete el campo descripcion";
+  }else{
+    $.ajax({
+      type: "POST",
+      url: "./php/phpinserts.php",
+      data: {
+        action: "GuardarTipoTramite",
+        Descripcion: txtdescripcionTramiteCreado.value,
+        estado: cbxestadoTramiteCreado.checked,
+      },
+      success: function (data) {
+        if (data == "esta") {
+          lblnotificacionagregado.innerText = "esta";
         } else {
-          lblnotificacionagregado.innerText = "Error BD";
+          if (data == "guardado") {
+            lblnotificacionagregado.innerText = "Se guardo correctamente";
+            ObtenerTipoTramites();
+          } else {
+            lblnotificacionagregado.innerText = "Error BD";
+          }
         }
-      }
-    },
-  });
+      },
+    });
+  }
 }
 
 function ObtenerTipoTramites() {
@@ -75,7 +101,7 @@ function ObtenerTipoTramites() {
       action: "ObtenerTipoTramites",
     },
     success: function (data) {
-      console.log(data);
+      /* console.log(data); */
       obtenerTipoTramites.innerHTML = data;
     },
     error: function (xhr, status, error) {
@@ -83,3 +109,38 @@ function ObtenerTipoTramites() {
     },
   });
 }
+
+function editarTramite(){
+  if(descripcionTramiteEditado.value =="") {
+    lblnotificacioneditado.innerText = "Complete el campo descripcion";
+  }else{
+    $.ajax({
+      type: "POST",
+      url: "./php/phpupdate.php",
+      data: {
+        action: "EditarTipotramite",
+        descripcion : descripcionTramiteEditado.value,
+        estado: estadoTramiteEditado.checked,
+        idtipotramite: lblIdTipotramite.textContent
+      },
+      success: function (data) {
+        console.log(data);
+        if (data == "esta") {
+          lblnotificacioneditado.innerText = "Ese nombre ya se encuentra";
+        } else {
+          if (data == "modificado") {
+            lblnotificacioneditado.innerText = "Se modificado correctamente";
+            ObtenerTipoTramites();
+          } else {
+            lblnotificacioneditado.innerText = "Error BD";
+          }
+        }
+      },
+      error: function (xhr, status, error) {
+        console.error("Error en la solicitud AJAX:", error);
+      },
+    });
+  }
+  
+}
+
